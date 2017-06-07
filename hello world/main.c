cf8a53387538ed3368b7f3792d733e83ec37e9ec
201611-4588 <1263107743@qq.com>
2017-06-07 10:26:52 +0800

    Initial Commit

---
+++ hello world/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  hello world
+//
+//  Created by 20161104588 on 17/6/7.
+//  Copyright © 2017年 20161104588. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
